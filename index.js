39621ab4f8f712fa0bb007ec4e52021b5e4379c5
FuTau <83931109+FuTau@users.noreply.github.com>
2024-08-05 10:37:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello Github Pages");
+document.addEventListener('DOMContentLoaded', (event) => {
+  const button = document.getElementById('changeColorBtn');
+  button.addEventListener('click', () => {
+    document.body.style.backgroundColor = getRandomColor();
+  });
+});
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
